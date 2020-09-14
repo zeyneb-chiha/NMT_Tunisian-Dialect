6b285531b62024355144602e03dfe39e5f3f1168
zeyneb chiha <53334878+zeyneb-chiha@users.noreply.github.com>
2020-09-14 17:07:26 +0100

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -1,8 +1,8 @@
-from model import nmt_ar2en
+from model import nmt_dt2ar
 
 if __name__ == "__main__":
 
     save_dir = 'model/model_save'
-    nmt = nmt_ar2en()
+    nmt = nmt_dt2ar()
     nmt.load_model(save_dir=save_dir)
     nmt.translate_api_response(u'kamel')
