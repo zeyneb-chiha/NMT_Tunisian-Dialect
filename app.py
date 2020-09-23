759af164e81869e23a41af70e3e7429441f213f7
zeyneb chiha <53334878+zeyneb-chiha@users.noreply.github.com>
2020-09-23 02:50:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask import Flask, request, send_from_directory, render_template, json
 
 app = Flask(__name__)
 
-save_dir = '/app/model/model_save'
+save_dir = 'C:/Users/hp/Desktop/internship2020/NMT att/machine-translation-nmt/model/model_save'
+
 
 nmt = nmt_dt2ar()
 nmt.load_model(save_dir=save_dir)
@@ -17,63 +18,34 @@ def favicon():
 
 
 
-@app.route('/robots.txt')
-def static_from_root():
-    return send_from_directory(app.static_folder, request.path[1:])
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", message='')
+    return render_template("index.html")
 
 
 
-@app.route("/", methods=['POST'])
-def move_forward():
-    DT_txt = request.form.get('arabic-text')
-    ara_txt = ''
-    try:
-       ara_txt,_,_ = nmt.translate_api_response(DT_txt)
-    except KeyError as e:
-        ara_txt = "Error with this word '%s', Check the spelling."%e
-    finally:
-        return render_template('index.html', message=[DT_txt, ara_txt])
 
 
-app.config['TRAP_HTTP_EXCEPTIONS']=True
-app.register_error_handler(Exception, defaultHandler)
+@app.route('/Translate', methods = ['POST','GET'])
+def Translate():
+    
+        if request.method == "POST":
+            DT=request.form["message"]
+            AR=nmt.translate(message)
+            return render_template('index.html', message=DT, output=AR)
+        else:
+            return render_template('index.html')
 
-"""
-@app.route('/api', methods=['GET'])
-def api():
-    isDataComplete = request.args.get("q") and request.args.get("sourceLang") and request.args.get("targetLang")
-    # check that all GET data need is received
-    if isDataComplete:
-        __GET__ = {"text": request.args.get("q"),
-                   "src": request.args.get("sourceLang"),
-                   "target": request.args.get("targetLang")}
 
-        translated = "translate \" %s \" from %s to %s " % (__GET__["text"], __GET__["src"], __GET__["target"])
-        translated_to, confidence, backend = nmt.translate_api_response(__GET__["text"])
-        response_json = {
-            "src": __GET__["src"],
-            "confidence": 1,
-            "sentences": [{
-                            "trans": translated_to,
-                            "orig": __GET__["text"],
-                            "backend": 1}],
-        }
 
-        response = app.response_class(
-            response=json.dumps(response_json),
-            status=200,
-            mimetype='application/json'
-        )
-        return response
 
-    return render_template('error.html')
-"""
+
+
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    app.run(debug=True)
+    app.config[SERVER_NAME]='localhost:5000'
